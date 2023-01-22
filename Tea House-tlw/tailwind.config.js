5d10595df2dc7574bb643644f1b562bfd6876226
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-01-23 01:42:30 +0600

    sc

--- Tea House-tlw/tailwind.config.js
+++ Tea House-tlw/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
 
     fontFamily: {
        'abc': ['Manrope',['sans-serif']]
-      }
+      },
+    //   droppShadow:{
+    //     'xl': '0 7px 50px rgba(5, 0, 0, 0.25)',
+        
+    //   }
 }
 
