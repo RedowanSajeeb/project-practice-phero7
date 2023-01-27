38b70b117414101b2578bcddce73c04d4d9d35e5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-01-28 01:51:09 +0600

    folder-cng

---
+++ javascript/Introduction-to-JavaScript/firsjs.js
@@ -0,0 +1,27 @@
+var time1 = 2023;
+var years =2025;
+var programin = 2;
+
+// console.log("time")
+console.log(programin);
+console.log(17%5)
+
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray[2]);
+
+let x = 5;
+let y = 10;
+if (x < y) {
+ console.log("x is less than y");
+} else {
+ console.log("x is greater than or equal to y");
+}
+let x = 5;
+let y = 10;
+if (x < y) {
+ console.log("x is less than y");
+} else {
+ console.log("x is greater than or equal to y");
+}
+console.log(6 != 6);
+console.log(6 >= 6);
