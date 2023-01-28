0013a35994f9604d76992bf422fff55ea70facf7
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-01-29 03:59:35 +0600

    pre4

---
+++ javascript/Practice Problem/m18/pra3.js
@@ -0,0 +1,20 @@
+// 1. You are given three numbers 13, 79, and 45. Write a program that will print the
+// largest number using if-else.
+// 2. You are given a triangle with the sides 9, 8, 9. Write a program to check whether a
+// triangle is Isosceles or not using if-else.
+
+// Isosceles => two sides are equal
+// ------------------------------------------------------
+var number1 = 13; 
+var number2 = 79; 
+var number3 = 45; 
+
+if (number1 > number2){
+    console.log(number1);
+}
+else if (number2 > number3){
+    console.log(number2);
+}
+else if (number2 && number2 < number3){
+    console.log(number3);
+}
