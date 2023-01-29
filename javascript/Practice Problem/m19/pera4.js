787f4dfc48000a314ac97c7a93080db4155fee89
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-01-30 04:16:23 +0600

    inshaAllah

--- javascript/Practice Problem/m19/pera4.js
+++ javascript/Practice Problem/m19/pera4.js
@@ -11,4 +11,4 @@ var PotiDinEr_kaj = [
         console.log(PotiDinEr_kaj);
         i++;
     }
-    // ------------javascript-vaja-vaja-korta-hobe-inSha
+    // ------------javascript-vaja-vaja-korta-hobe-inSha-Allah
