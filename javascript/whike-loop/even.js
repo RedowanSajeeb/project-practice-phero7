f90ce205fb8f6300bbfbb4424bb49592924557f8
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-01-29 09:39:42 +0600

    While-loop

---
+++ javascript/whike-loop/even.js
@@ -0,0 +1,11 @@
+var EvenNumber = 0;
+while (EvenNumber <=50 ){
+    console.log(EvenNumber);
+    EvenNumber += 2; 
+}
+// -------------odd
+var EvenNumber = 1;
+while (EvenNumber <=50 ){
+    console.log(EvenNumber);
+    EvenNumber += 2; 
+}
