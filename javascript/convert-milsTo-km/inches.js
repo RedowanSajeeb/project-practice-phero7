0e6c6390c6067e05d7e81a5c558adddf9af6873b
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-04 01:32:01 +0600

    inc

---
+++ javascript/convert-milsTo-km/inches.js
@@ -0,0 +1,8 @@
+function inchesConvert(mitterParamiter){
+    const Output = mitterParamiter * 39.3700787402;
+    return Output ;
+}
+
+const mitter_outpust_option = 2;
+const consoleeLog = inchesConvert (mitter_outpust_option);
+console.log ("output", consoleeLog);
