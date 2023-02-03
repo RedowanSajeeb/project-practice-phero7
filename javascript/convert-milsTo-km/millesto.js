c18f08102e8aaaeb0c9fe6a9fa6c8d67125d6672
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-04 01:25:25 +0600

    mill

---
+++ javascript/convert-milsTo-km/millesto.js
@@ -0,0 +1,7 @@
+ function MillesTokmConvert(milll){
+    const input = milll*1.60934;
+    return input;
+ }
+ const OustsideMills = 2;
+ const callMills = MillesTokmConvert(OustsideMills);
+ console.log("Output", callMills);
