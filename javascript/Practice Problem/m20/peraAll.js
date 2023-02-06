3a316cda6670c5e2b14008ed28101ee4adccb039
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-06 17:28:06 +0600

    index

--- javascript/Practice Problem/m20/peraAll.js
+++ javascript/Practice Problem/m20/peraAll.js
@@ -46,3 +46,30 @@ function square(input){
 }
 let outpue = square(5);
 console.log(outpue);
+
+// ---------------------
+// ৫) pizza নামের একটা object কে নিচের উদাহরণ মতো define করবা :
+
+// const pizza = {
+
+// toppings: ['cheese', 'sauce', 'pepperoni'],
+
+// crust: 'deep dish',
+
+// serves: 2
+
+// }
+
+// এবং pepperoni প্রিন্ট করবা।
+
+
+const pizza = {
+
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    
+    crust: 'deep dish',
+    
+    serves: 2
+    
+    }
+    console.log(pizza.toppings[2]);
