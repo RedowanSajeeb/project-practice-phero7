e7732243f4aa595c379ae44e7e857465e6d6a35b
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-06 17:43:44 +0600

    odd-even

--- javascript/Practice Problem/m21/perk.js
+++ javascript/Practice Problem/m21/perk.js
@@ -19,3 +19,18 @@ function leapYear(dateY){
     }
 }
 let datacaall = leapYear(2023);
+
+// ২. তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে।
+// ----------------
+
+function evenOdd(EvenYer){
+    if( EvenYer % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+let Consl= evenOdd(18);
+console.log(Consl);
