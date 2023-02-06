9f1b1b5bbd5f74d0a2d197c0b7477abf238c24a7
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-06 23:30:40 +0600

    sol2

--- javascript/Assignment-Javascript/assignment4.js
+++ javascript/Assignment-Javascript/assignment4.js
@@ -3,7 +3,6 @@
  function Name “mindGame()” & input- positive number multiply that number by 3, then add 10, then divide by 2, then subtract 5, and return the output.
  --------------------------------------------
 */
-
 function mindGame(InputPositiveNumber){
     if (typeof InputPositiveNumber == "number"){
         if( InputPositiveNumber < 0){
@@ -20,7 +19,37 @@ function mindGame(InputPositiveNumber){
     }
 
 }
-
-const OutpuResultCallHand = mindGame ("50")
+const OutpuResultCallHand = mindGame (50)
 console.log (OutpuResultCallHand);
-// ------------------------------------------------------------------
+
+
+// --------------------------------------
+/*solving-Problem 2:Finding even or odd 
+-----------------------------------------
+ function name And  function Called evenOdd() & input- string Type The output should be given based on the total number of characters in the String. Output will be 'even' or 'odd'.
+ -------------------------------------------------------------------------------
+*/
+
+function evenOdd(InputString){
+    if (typeof InputString == "string"){
+        let StringCharacterslength = InputString.length;
+        if( StringCharacterslength % 2 == 0 ){
+            return "even";
+        }
+        else {
+            return "odd"
+        }
+    }
+    else {
+        return "plz-input Push TypeOf-String likewise:'Phero7' "
+    }
+}
+
+const ouput2 = evenOdd ('Phero');
+console.log(ouput2);
+// ------------------------------------------------
+
+
+/*solving-Problem 3:
+
+*/
