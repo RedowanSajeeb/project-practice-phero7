453339d9f86e4dfafb60a9f20ecd9b86dcc9b6b5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-06 17:11:52 +0600

    upa

--- javascript/Practice Problem/m20/peraAll.js
+++ javascript/Practice Problem/m20/peraAll.js
@@ -13,3 +13,16 @@ let n = 13;
                 }
             }
             NamtaFuntion(13);
+
+
+            // ২. একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে।
+            // --------------
+
+            function Namcase (sentence){
+                console.log(sentence.toUpperCase());
+            }
+            Namcase("redowan-sajeeb")
+
+
+            // -----------------
+            
