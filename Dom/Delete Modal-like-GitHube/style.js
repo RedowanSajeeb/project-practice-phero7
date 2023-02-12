2da9736a31a4106c02c5d2922c7b47f2059bfea6
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-12 06:43:53 +0600

    nre

--- Dom/Delete Modal-like-GitHube/style.js
+++ Dom/Delete Modal-like-GitHube/style.js
@@ -1,18 +1,45 @@
 
 document.getElementById('inputBoxJs').addEventListener('keyup',function(eventtext){
      const textConf = eventtext.target.value;
+     
+     
        const  confmBtndlt = document.getElementById('inputBtnJs');
         if(textConf === 'redowan'){
             confmBtndlt.removeAttribute('disabled');
 
         }
+        
         else {
             confmBtndlt.setAttribute('disabled', true);
         }
+        
 })
 
 document.getElementById('inputBtnJs').addEventListener("click",function(){
     const dltsectionText = document.getElementById('dlt-sectionJs');
      dltsectionText.style.display ='none';
+     
+     const valoremor = document.getElementById('inputBoxJs');
+     valoremor.value = " ";
+})
+
+
+// ----------------
+document.getElementById('inputBoxJs').addEventListener('keyup',function(eventtext){
+    const textConf = eventtext.target.value;
+      const  confmBtndlt = document.getElementById('inputBtnJs');
+       if(textConf === 'sajeeb'){
+           confmBtndlt.removeAttribute('none');
+
+       }
+       
+       else {
+           confmBtndlt.setAttribute('non', true);
+       }
+})
+document.getElementById('inputBtnJs').addEventListener("click",function(){
+    const dltsectionText = document.getElementById('dlt-sectionJs');
+     dltsectionText.style.display ='none' ;
 
 })
+// -----------
