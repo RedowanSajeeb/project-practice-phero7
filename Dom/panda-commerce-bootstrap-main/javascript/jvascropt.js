5d674be0126897bd53404c08acc5d1d0b85c0ee5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-13 02:21:24 +0600

    abr-ow-daka-hobe

--- Dom/panda-commerce-bootstrap-main/javascript/jvascropt.js
+++ Dom/panda-commerce-bootstrap-main/javascript/jvascropt.js
@@ -6,7 +6,8 @@ for( const elements of h2colorvar){
 
 
 const bgcolor = document.getElementById('backpack');
-bgcolor.style.backgroundColor ="tomato";
+    bgcolor.style.backgroundColor ="tomato" ;
+    bgcolor.style.paddingBottom = "35px" ;
 // -----------------------
 
 
@@ -18,3 +19,57 @@ const cardstil = document.getElementsByClassName('card');
 //  -----------------
 
 
+function functionbtn () {
+  
+    document.body.style.backgroundColor = "lightblue";
+    
+}
+// -----------------parlam nah......
+
+const allbtn = document.getElementsByClassName('btn panda-btn-buy-now');
+    for(const onebtn of allbtn){
+        document.getElementById(onebtn).addEventListener('click',function(event){
+            const caraccses = document.getElementsByClassName('card');
+            for(const carremove of event ){
+                caraccses.style.display = 'none';
+            }
+        })
+        
+        
+    }
+
+
+    
+
+    // -------------------------
+
+    document.getElementById('InputEmail1').addEventListener('keyup',function(event){
+        const inputfildTextDo = event.target.value;
+        const btnpoint = document.getElementById('b');
+        if(inputfildTextDo == "email"){
+            btnpoint.removeAttribute('disabled');
+        
+        }
+        else {
+            btnpoint.setAttribute('disabled', true);
+        }
+
+
+    })
+
+
+   
+    
+      
+// document.getElementById('exampleInputEmail1').addEventListener('keyup',function(eventtxt){
+//    const textin = eventtxt.target.value;
+
+//     const btOneee  = document.getElementById('exampleInputbtn');
+//     if(textin === 'email'){
+//         btOneee.removeAttribute('disabled');
+//     }
+//     else {
+//         btOneee.setAttribute('disabled', true);
+//     }
+    
+// })
