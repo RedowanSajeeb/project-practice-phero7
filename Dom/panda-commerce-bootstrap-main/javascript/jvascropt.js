1c51b5574f8a610ee0e33a974a7585d13fbe0a44
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-12 22:38:50 +0600

    2

---
+++ Dom/panda-commerce-bootstrap-main/javascript/jvascropt.js
@@ -0,0 +1,20 @@
+const h2colorvar = document.getElementsByTagName('h2');
+for( const elements of h2colorvar){
+    elements.style.color = "lightblue";
+}
+// ---------------
+
+
+const bgcolor = document.getElementById('backpack');
+bgcolor.style.backgroundColor ="tomato";
+// -----------------------
+
+
+
+const cardstil = document.getElementsByClassName('card');
+  for(const cardbr of cardstil){
+    cardbr.style.borderRadius = '30px';
+  }
+//  -----------------
+
+
