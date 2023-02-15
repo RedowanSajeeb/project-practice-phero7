5a52e71091a87c170539a179f716ed5c23ae3cd0
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-15 22:58:51 +0600

    done

---
+++ Dom/discount-coupon/Js/discount.js
@@ -0,0 +1,15 @@
+document.getElementById('discount-btn').addEventListener('click',function(){
+    const firstPricInput = pricInputValu('enter-price-fild');
+    const priceInputGrowPrice = priceInner('firsttalprice');
+    const documentPrice30 = firstPricInput - (firstPricInput * 0.3) ;
+    paramiterCallWithFuntion('firsttalprice', documentPrice30);
+
+    const pomoCodeValue = pricInputValu ('input-field');
+    if(pomoCodeValue = 'DISC30'){
+        
+    }
+
+
+
+
+})
---
+++ Dom/discount-coupon/Js/utilities.js
@@ -0,0 +1,20 @@
+function pricInputValu (inputPrice){
+    const getElementByIdCount = document.getElementById(inputPrice);
+    const stringPriceValu = getElementByIdCount.value;
+    const inputvalu = parseFloat(stringPriceValu);
+    // stringPriceValu.value = ' ';
+    return inputvalu ;
+}
+function paramiterCallWithFuntion(paramiter, setCall){
+
+    const paramiterIdgetCall = document.getElementById(paramiter);
+    paramiterIdgetCall.innerText = setCall;
+}
+
+function priceInner (priceInnerParametar){
+    const getElementByIdCountInnerText = document.getElementById(priceInnerParametar);
+    const stringPriceValuInner = getElementByIdCountInnerText.innerText;
+    const inputvaluInner = parseFloat(stringPriceValuInner);
+    
+    return inputvaluInner;
+}
