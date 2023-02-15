0915a8fb48598cabc70dd338f6e36163d7cb59b0
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-15 09:43:52 +0600

    new-project

---
+++ Dom/commonFuntion-Bapp Bank/JS/login.js
@@ -0,0 +1,14 @@
+document.getElementById('Submit-button').addEventListener('click',function(){
+    const emailUser = document.getElementById('user-email-input');
+    const email = emailUser.value;
+    
+    const passUser = document.getElementById('user-password-input');
+    const  password = passUser.value;
+    if(email == "a" && password ==='a'){
+        window.location.href = 'bankindex.html'
+        
+    }
+    else{
+        alert("InvalidBro");
+    }
+})
