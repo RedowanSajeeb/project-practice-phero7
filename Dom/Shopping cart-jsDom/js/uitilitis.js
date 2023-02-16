dc9fbc726d8a269f9c3768cefcc93f50448da6e6
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-17 04:38:35 +0600

    done

--- Dom/Shopping cart-jsDom/js/uitilitis.js
+++ Dom/Shopping cart-jsDom/js/uitilitis.js
@@ -4,7 +4,7 @@
  const inputPureValue = parseInt(inputValue);
  return inputPureValue;
  }
- 
+
 //  set-price---------
 function priceSet(iphoneprice){
     const priceId = document.getElementById(iphoneprice);
@@ -22,3 +22,41 @@ function getSetpriceValue(setIDsec, finnalset){
     const getLocationprice = document.getElementById(setIDsec);
     getLocationprice.value = finnalset;
 }
+
+
+
+
+
+
+
+
+// function getTextElementValueById(elementId){
+//     const phoneTotalElement = document.getElementById(elementId);
+//     const currentPhoneTotalString = phoneTotalElement.innerText;
+//     const currentPhoneTotal = parseInt(currentPhoneTotalString);
+//     return currentPhoneTotal;
+// }
+
+// function setTextElementValueById(elementId, value){
+//     const subTotalElement = document.getElementById(elementId);
+//     subTotalElement.innerText = value;
+// }
+
+// function calculateSubTotal(){
+//     // calculate total
+//     const currentPhoneTotal = getTextElementValueById('phone-total');
+//     const currentCaseTotal = getTextElementValueById('case-total');
+    
+//     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+//     setTextElementValueById('sub-total', currentSubTotal);
+
+//     // calculate tax
+//     const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+//     const taxAmount = parseFloat(taxAmountString);
+//     setTextElementValueById('tax-amount', taxAmount);
+
+//     const finalAmount = currentSubTotal + taxAmount;
+//     setTextElementValueById('final-total', finalAmount);
+// }
+
+
