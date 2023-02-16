082ab0e1fd8118c5f0c28056279ee73512ef49e7
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-16 17:41:09 +0600

    done

--- Dom/discount-coupon/Js/discount.js
+++ Dom/discount-coupon/Js/discount.js
@@ -2,23 +2,27 @@ document.getElementById('discount-btn').addEventListener('click',function(){
 
     const firstPricInput = pricInputValu('enter-price-fild');
         paramiterCallWithFuntion('firsttalprice',firstPricInput); 
-        
+
 })
 document.getElementById('confmbtn').addEventListener('click', function() {
-    const confromIDget = document.getElementById('confmbtn');
-    if (confromIDget == 'DISC30') {
+    const confromIDget = document.getElementById('inputfield').value
+    console.log(confromIDget);
+        if (confromIDget == 'DISC30') {
       const priceInputGrowPrice = priceInner('firsttalprice');
     //   -----------------------------------------------------------------------
       const documentPrice30 = priceInputGrowPrice - (priceInputGrowPrice * 0.3);
     //   ------------------------------------------------------------------------
       const documentPrice3080 = documentPrice30 + 80;
       paramiterCallWithFuntion('total-payment', documentPrice3080);
-    } else {
-      const priceInputGrowPrice = priceInner('firsttalprice');
-    //   const price = priceInputGrowPrice + 80;
-    const documentPrice30 = priceInputGrowPrice - (priceInputGrowPrice * 0.3);
-    const documentPrice3080 = documentPrice30 + 80;
-      paramiterCallWithFuntion('total-payment', documentPrice3080);
+    } else  {
+
+
+      
     }
   });
+  document.getElementById('place-order-btn').addEventListener('click',function(){
+
+    // const getElementByIdif = document.getElementById('place-order-btn');
+    window.location.href ="oder.html";
+  })
   
