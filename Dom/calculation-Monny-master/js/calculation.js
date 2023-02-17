bbb67019f646890e8bcdb4eb63ce330c19bb3ebc
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-17 16:42:12 +0600

    all-done

--- Dom/calculation-Monny-master/js/calculation.js
+++ Dom/calculation-Monny-master/js/calculation.js
@@ -4,10 +4,36 @@ document.getElementById('calculator-bbtn').addEventListener('click',function(){
    const foodIncome = incomeinputBoxGetid('food-input');
    const rentIncome = incomeinputBoxGetid('rent-input');
    const clothIncome = incomeinputBoxGetid('cloth-input');
-
+// ---------------------------
+   if(isNaN(firstBoxBtnIncome)){
+   alert ('plz-input-Taka');
+   return;
+   }
+//    --------------------
+   if(firstBoxBtnIncome < 0){
+    alert('plz-positive-$')
+    return;
+   }
+   if(foodIncome < 0){
+    alert('plz-positive-$')
+    return;
+   }
+   if(rentIncome < 0){
+    alert('plz-positive-$')
+    return;
+   }
+   if(clothIncome < 0){
+    alert('plz-positive-$')
+    return;
+   }
+//    ---------------------------
    const calculationFirstBtn = foodIncome + rentIncome + clothIncome;
    const fistCalculation = firstBoxBtnIncome - calculationFirstBtn;
-   
+
+   if(calculationFirstBtn > firstBoxBtnIncome){
+    alert('kom kom taka nosto koro')
+   }
+
    getSetLocationIDCall('display-balance',fistCalculation);
    getSetLocationIDCall('display-expense',calculationFirstBtn);
 
@@ -15,8 +41,20 @@ document.getElementById('calculator-bbtn').addEventListener('click',function(){
    
 document.getElementById('save-btn').addEventListener('click',function(){
     const savinginputget = incomeinputBoxGetid('saving-input');
+// ---------------------------------
+    if(isNaN(savinginputget)){
+        alert ('plz-input-Saving%');
+        return;
+        }
+        // ------
+         if(savinginputget > 100){
+    alert('plz-100%+')
+    return;
+   }
+        // ---------------
     const firstBoxBtnIncome = incomeinputBoxGetid('income-input');
     const savings = firstBoxBtnIncome * savinginputget / 100;
+ 
     getSetLocationIDCall('display-saving',savings);
     // display-remaining
     const firstBoxBtnIncomee = incomeinputBoxGetid('income-input');
