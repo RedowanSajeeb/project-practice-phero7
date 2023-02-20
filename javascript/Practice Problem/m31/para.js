8cdf3ef7a0f54ddffe3afaf46f2ea4a5cfe29b1e
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-21 04:57:36 +0600

    para

---
+++ javascript/Practice Problem/m31/para.js
@@ -0,0 +1,28 @@
+// Practice Problem 1
+// Write an arrow function that will take 3 parameters, will multiply
+// the parameters and will return the result.
+// ------
+// 1.
+const multiplyFuntion = (a, b, c) => a * b * c;
+const result = multiplyFuntion(5,2,5);
+console.log(result);
+
+// 2
+// Write the following sentence in three lines and print the result:
+// I am a web developer. I love to code. I love to eat biryani.
+const threeLin = `I am a web developer.
+I love to code.
+I love to eat biryani.
+`
+console.log(threeLin);
+
+// 3) Write an arrow function that will take 2 parameters: One parameter
+// will come from you and the other parameter will be a default
+// parameter. Add these two parameters and return the result.
+
+ const defaultParamiterFuntion = (a, b = 20) => a + b;
+ console.log(defaultParamiterFuntion(5))
+
+
+//  Practice Problem 2
+
