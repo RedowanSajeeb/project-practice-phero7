b80ae96962a11fe64b6aa3ee1c8810afdffef057
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-22 09:35:44 +0600

    sum

---
+++ javascript/Practice Problem/m32/sec.js
@@ -0,0 +1,26 @@
+// Practice Problem reduce()
+/* 1) You have an array [ 1, 9, 17, 22 ]. Add the all elements
+of this array and give output. Do this using for loop &
+array.reduce() method. */
+
+const anArray = [ 1, 9, 17, 22 ];
+const reduceArray = anArray.reduce((total, ammount) => total + ammount);
+// console.log(reduceArray);
+
+const sum = 0;
+const anArray1 = [ 1, 9, 17, 22 ];
+const reduceArray1 = anArray1.reduce((total1, ammount1) => total1 + ammount1,sum);
+// console.log(reduceArray1);
+
+// ----------------------
+// for loop
+const anArray1forloop = [ 1, 9, 17, 22 ];
+    let same = 0;
+for (const forloopOf of anArray1forloop){
+    
+        same += forloopOf
+        // same = forloopOf + same
+}
+   console.log(same);
+
+// -----------------
