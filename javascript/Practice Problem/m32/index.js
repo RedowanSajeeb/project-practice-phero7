d0acb9193001db730883466eae175028b6677c3c
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-22 08:52:15 +0600

    add

---
+++ javascript/Practice Problem/m32/index.js
@@ -0,0 +1,19 @@
+/* Practice Problem map(), filter(), find()*/
+/* 1) You have an odd array (array with odd numbers). [ 1, 3, 5, 7, 9 ].
+Now convert this array into an even array (array with even
+numbers). [ 2, 4, 6, 8, 10 ]. Do this using for loop & array.map()
+method. Hints: add one to any odd number and it will become an even
+number. */
+const oddArray = [ 1, 3, 5, 7, 9 ];
+const evenAodd =[];
+for(const evenAray of oddArray){
+    const evenNumber = evenAray +1;
+    evenAodd.push(evenNumber);
+}
+console.log(evenAodd);
+// ----------------------Math.map-------
+const oddArrayMap =  [ 1, 3, 5, 7, 9 ];
+const result = oddArrayMap.map(x => x+1);
+console.log(result);
+// --------------------
+
