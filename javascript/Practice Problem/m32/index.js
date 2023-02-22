4375ca4e6f937e69e2cec7210e394a9f4ec7f9b5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-22 09:08:36 +0600

    %

--- javascript/Practice Problem/m32/index.js
+++ javascript/Practice Problem/m32/index.js
@@ -10,10 +10,19 @@ for(const evenAray of oddArray){
     const evenNumber = evenAray +1;
     evenAodd.push(evenNumber);
 }
-console.log(evenAodd);
+// console.log(evenAodd);
 // ----------------------Math.map-------
 const oddArrayMap =  [ 1, 3, 5, 7, 9 ];
 const result = oddArrayMap.map(x => x+1);
-console.log(result);
+// console.log(result);
+
+
 // --------------------
 
+/* 2) You are given an array say: [33, 50, 79, 78, 90, 101, 30 ]. Now
+return/get all the elements which are divisible by 10 using
+array.filter() method.
+ */
+const givenSayAray = [33, 50, 79, 78, 90, 101, 30 ];
+const sayresult = givenSayAray.filter(x => x % 10 === 0 );
+// console.log(sayresult);
