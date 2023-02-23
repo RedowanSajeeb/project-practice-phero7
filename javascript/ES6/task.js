b3b5dc7c79a2053357eccf5c5801c54307d247ed
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-24 03:50:07 +0600

    3.1

---
+++ javascript/ES6/task.js
@@ -0,0 +1,18 @@
+// 1
+const cngdHoyaNah = "redowan";
+let wather = "50dc";
+
+// 2
+const boj = {
+    name:"redo1",
+    secName:'sajeeb2'
+}
+ const tmString = `
+  First Pershon Name ${cngdHoyaNah} Done Wather ${wather} and obj ${boj.name.boj} && ${boj.secName}
+ 
+ `
+ console.log(tmString);
+
+// 3.1
+
+
