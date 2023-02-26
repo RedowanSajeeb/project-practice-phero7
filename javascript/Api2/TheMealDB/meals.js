913626c0ca76a34577579a8899e3efe57f7ef80b
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-26 09:40:14 +0600

    all-done-bro

--- javascript/Api2/TheMealDB/meals.js
+++ javascript/Api2/TheMealDB/meals.js
@@ -1,12 +1,17 @@
-const firstMealdbLoad = () => {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=cake")
+const firstMealdbLoad = (serBoxText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${serBoxText}`
+  fetch(url)
+  // fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+
     .then((res) => res.json())
     .then((data) => displayMeals(data.meals));
 };
 
 const displayMeals = (meals) => {
   const mealscontainer = document.getElementById("mealsContainer");
-
+  // ----------------------
+  mealscontainer.innerHTML = ' ';
+// ----------------------------
   meals.forEach((meal) => {
     const divmeal = document.createElement("div");
     divmeal.classList.add("col");
@@ -15,15 +20,26 @@ const displayMeals = (meals) => {
     <div class="card h-100">
     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${meal.strMeal}</h5>
+      <h4 class="card-title text-success">${meal.strMeal}</h4>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
   </div>
-        
-        `;
+        `
 
     mealscontainer.appendChild(divmeal);
   });
 };
+
+const mealsearearch = () =>{
+  const seboxid = document.getElementById('example-search-input').value;
+  const seboxidemt = document.getElementById('example-search-input')
+
+  firstMealdbLoad(seboxid);
+
+   seboxidemt.value = " ";
+}
+
+
+
 
 firstMealdbLoad();
