16c4313820270f5c9bce7f35a625e18c0048e1a1
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-26 11:47:35 +0600

    done

--- javascript/Api2/TheMealDB/meals.js
+++ javascript/Api2/TheMealDB/meals.js
@@ -22,14 +22,17 @@ const displayMeals = (meals) => {
     <div class="card-body">
       <h4 class="card-title text-success">${meal.strMeal}</h4>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <!-- Button trigger modal -->
+      <button onclick="loadMealdetails(${meal.idMeal})" type="button" class="btn btn-primary" 
+      data-bs-toggle="modal" data-bs-target="#mealdetailsModel">
+        Launch demo modal
+      </button>
     </div>
   </div>
         `
-
     mealscontainer.appendChild(divmeal);
   });
 };
-
 const mealsearearch = () =>{
   const seboxid = document.getElementById('example-search-input').value;
   const seboxidemt = document.getElementById('example-search-input')
@@ -39,7 +42,29 @@ const mealsearearch = () =>{
    seboxidemt.value = " ";
 }
 
+const loadMealdetails = idMeal =>{
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => desplaymeldetails(data.meals[0]))
+}
 
+const desplaymeldetails = meal =>{
+  document.getElementById('mealdetailsModelLabel').innerText = meal.strMeal
+
+  const dtails = document.getElementById('dtails');
+  dtails.innerHTML = `
+  <img class ="img-fluid" src="${meal.strMealThumb}" alt="xphoto" >
+  <p class ="mt-3">${meal.strInstructions}</p>
+  
+  `
+
+
+
+  // document.getElementById('dtails').innerText = meal.strInstructions
+ 
+
+}
 
 
 firstMealdbLoad();
