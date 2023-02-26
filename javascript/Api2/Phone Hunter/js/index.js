5d1897eaa464898ce5081585bfb097a7d37bb028
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-26 18:43:31 +0600

    phone-hunter

--- javascript/Api2/Phone Hunter/js/index.js
+++ javascript/Api2/Phone Hunter/js/index.js
@@ -1,30 +1,34 @@
-const phonsLoadHunt = async(phoneSearchBox) =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearchBox}`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayPhonesHunts(data.data);
-}
+const phonsLoadHunt = async (phoneSearchBox, dataLemit) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${phoneSearchBox}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhonesHunts(data.data, dataLemit);
+};
+const displayPhonesHunts = (phones, dataLemit) => {
+  const phoneContainar = document.getElementById("phoneContainner");
+  phoneContainar.textContent = "";
+  const showAll = document.getElementById("btn-show-all");
+  if (dataLemit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
 
-const displayPhonesHunts = phones =>{
-    const phoneContainar = document.getElementById('phoneContainner');
-    phoneContainar.textContent ="";
-    phones = phones.slice(0,25);
-    /* desplay-30+Phones onaly *
+  /* desplay-30+Phones onaly *
     // -------------------------------------
-    
     /* -------------error--no-phone */
-   const dblocPage = document.getElementById('d-bloc-section');
-     if(phones.length === 0){
-        dblocPage.classList.remove('d-none')
-     }
-     else{
-        dblocPage.classList.add('d-none')
-     }
-// ------------------------
-    phones.forEach(phone => {
-        const phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('col');
-        phoneDiv.innerHTML = `
+  const dblocPage = document.getElementById("d-bloc-section");
+  if (phones.length === 0) {
+    dblocPage.classList.remove("d-none");
+  } else {
+    dblocPage.classList.add("d-none");
+  }
+  // ------------------------
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
 
         <div class="card p-4">
         <img  src="${phone.image}" class=" img-fluid p-5" alt="...">
@@ -33,27 +37,43 @@ const displayPhonesHunts = phones =>{
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
-        `
-        phoneContainar.appendChild(phoneDiv)
+        `;
+    phoneContainar.appendChild(phoneDiv);
+  });
+  /* spinner-loder-stop */
+  spinnerLoder(false);
+};
 
-    });
-}
-document.getElementById('btn-click').addEventListener('click', function(){
-    const seachinputValu = document.getElementById('example-search-input').value;
-    const emtay = document.getElementById('example-search-input')
-    phonsLoadHunt(seachinputValu);
+const prossesSearch = (dataLemit) => {
+  spinnerLoder(true);
+  const seachinputValu = document.getElementById("example-search-input").value;
+  phonsLoadHunt(seachinputValu, dataLemit);
+};
+document.getElementById("btn-click").addEventListener("click", function () {
+  /*  spinner-loder-start */
+  prossesSearch(10);
+  // const emtay = document.getElementById('example-search-input')
 
-    emtay.value = " ";
+  // emtay.value = " ";
 });
 
-    
-   
+/*     spinner-loder
+ */
+const spinnerLoder = (loder) => {
+  const spinnerSection = document.getElementById("spinner-loder");
+  if (loder) {
+    spinnerSection.classList.remove("d-none");
+  } else {
+    spinnerSection.classList.add("d-none");
+  }
+};
+// -----------
+// btn-show-all
+document.getElementById("btn-show-btn").addEventListener("click", function () {
+  prossesSearch();
 
+  const emtay = document.getElementById("example-search-input");
+  emtay.value = " ";
+});
 
-
-
-
-
-
-
-phonsLoadHunt('a');
+phonsLoadHunt("a");
