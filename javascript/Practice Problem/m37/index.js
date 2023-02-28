3768bdc129cf5b9156edb7849a7ea6a2d9dec3ed
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-02-28 18:25:54 +0600

    interv

--- javascript/Practice Problem/m37/index.js
+++ javascript/Practice Problem/m37/index.js
@@ -79,3 +79,25 @@ performArithmeticOperation(10, 5, 'modulus'); // Output: 0
 performArithmeticOperation(10, 5, 'invalid'); // Output: Invalid operation
 
 // c------g---p-t-e1end---
+
+
+
+
+
+
+
+C/* ommon Interview Questions
+
+1) Tell the difference Between Primitive and Non-Primitive Data Types in JavaScript.
+2) What are the Truthy and Falsy values? Give me some examples.
+3) What is the difference between null and undefined?
+4) What are the differences between double equal (==) and triple equal (===)?
+5) What is scope in JavaScript?
+6) Define block scope and global scope.
+
+COMMON INTERVIEW QUESTIONS
+
+7) What is hoisting in JavaScript?
+8) How to use the JavaScript callback function?
+9) Explain closure in JavaScript
+10) Explain passed by value and passed by reference. */
