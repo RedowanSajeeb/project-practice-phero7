52aff5af57393ad3a22d90cccb566f9b7f1412c3
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-01 21:31:54 +0600

    done

--- javascript/Api2/Billionaire/Js/index.js
+++ javascript/Api2/Billionaire/Js/index.js
@@ -20,19 +20,23 @@ const billionairesManCall = (mans) => {
                       <div class="col-md-8 border-start">
                         <div class="card-body">
                          
-                          <h5 class="card-title">State: ${man.state}</h5>
+                          <h5 class="card-title">State: ${man.state? man.state : "No State"}</h5>
                           <h5 class="card-title">City: ${man.city}</h5>
                           <h5 class="card-title">Total Shares: ${man.financialAssets[0].numberOfShares}</h5>
                           <h5 class="card-title">Share price ${man.financialAssets[0].sharePrice}</h5>
                        
                           <h6 class="card-text">source: <small class="">${man.source}</small></h6>
-                        </div>
+
+                          <button onclick= "lodModalDetails()" type="button" class="btn btn-primary mt-2" style="background-color: #0E1B34;" data-bs-toggle="modal" data-bs-target="#hardWorkCillmodelid">General Information<button>
+                    </div>
                       </div>
                     </div>
                   </div>
         `;
     cardIDget.appendChild(setCart);
+    
   });
+
 };
 
 // loadMoreBtn
@@ -42,5 +46,6 @@ document.getElementById("loadMoreBtn").addEventListener("click", function () {
   timeBillionaires("limit=400");
 });
 // myFunctionmodal()
+
 
 timeBillionaires("limit=10");
