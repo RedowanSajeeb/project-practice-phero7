8f7e65e6d7c6649a526708dcc9e9feb198d926d3
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-07 20:13:55 +0600

    done

--- javascript/Debug-js/TimeCount/App/app.js
+++ javascript/Debug-js/TimeCount/App/app.js
@@ -1,10 +1,15 @@
-function startBtnFun(){
-const setTime = document.getElementById('time')
+let intervalId;
 let num = 0;
-const intervalId =  setInterval(()=>{
-    setTime.innerText = ++num
-   },1000);
-   
-
+const setTime = document.getElementById("time");
+function startBtnFun() {
+  intervalId = setInterval(() => {
+    setTime.innerText = ++num;
+  }, 1000);
 }
-
+function stopBtnfun() {
+  clearInterval(intervalId);
+}
+function resetBtnfun() {
+  clearInterval(intervalId);
+  setTime.innerText = 00;
+}
