4beae2124294140766a032ef6d4d0ccce1a7ef94
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-24 05:59:43 +0600

    done

--- Milestone-08-React/props/src/App.js
+++ Milestone-08-React/props/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       {idd.map((i) => (
-        <BestTopName>name={i}</BestTopName>
+        <BestTopName name={i}></BestTopName>
       ))}
       {idd.map((i) => (
         <li>name={i}</li>
