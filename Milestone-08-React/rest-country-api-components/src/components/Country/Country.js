9e41ed660a6490266be8e6efc978ab7c73cbb962
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-25 17:55:53 +0600

    child component

--- Milestone-08-React/rest-country-api-components/src/components/Country/Country.js
+++ Milestone-08-React/rest-country-api-components/src/components/Country/Country.js
@@ -3,14 +3,21 @@ import './Country.css'
 
 const Country = (props) => {
     // console.log(props.country.area);
+    const { capital, area, name } = props.cCountry;
     return (
       <div className="coyntry">
-        <h1>Country name: {props.cCountry.name.common}</h1>
+        <h1>Country name: {name.common}</h1>
+        <p>area{area}</p>
+        <h4>
+          Capital: {capital}
+          {/* <img src="{'props.flags'}" alt="" /> */}
+        </h4>
+        {/* <h1>Country name: {props.cCountry.name.common}</h1>
         <p>area{props.cCountry.area}</p>
         <h4>
           Capital: {props.cCountry.capital}
-          {/* <img src="{'props.flags'}" alt="" /> */}
-        </h4>
+          <img src="{'props.flags'}" alt="" />
+        </h4> */}
       </div>
     );
 };
