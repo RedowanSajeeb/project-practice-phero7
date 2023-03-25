216022ae5ff6a40aa78add44caf22d9650aed05d
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-26 02:42:21 +0600

    blog

---
+++ Milestone-08-React/paractice-day/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Article from './components/article/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <Article></Article>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Milestone-08-React/paractice-day/src/components/article/Article.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Article = () => {
+    return (
+      <div>
+        <article>
+          <h1>hey bloge</h1> <hr /> Whether you want to start a blog, or you’ve already
+          got one, it’s always good to peer over the fence to see what your
+          fellow bloggers are doing. Seeing other blog examples can spark
+          inspiration for your own blog. Plus, you can see what you’re up
+          against! For this article, we spent way too long scouring the internet
+          to bring you 43 of the best blog examples from many different niches
+          and built using a range of platforms. We hope this inspires you to see
+          what it’s possible to do with your own blog. Eat your heart out!
+        </article>
+      </div>
+    );
+};
+
+export default Article;
