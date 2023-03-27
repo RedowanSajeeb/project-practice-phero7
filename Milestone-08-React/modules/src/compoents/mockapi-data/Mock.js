ffaa37290dd9369d7c2f6ca0c75337ef92c925fa
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-27 07:09:17 +0600

    addTOCard

--- Milestone-08-React/modules/src/compoents/mockapi-data/Mock.js
+++ Milestone-08-React/modules/src/compoents/mockapi-data/Mock.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import './mock.css'
 const Mock = (props) => {
     const { id, name, price, color } = props.cosmatick;
-   
+   const addTocard = (id) =>{
+    console.log('button',id);
+
+   }
+//    const comment = () => addTocard(id)
+
     return (
-      <div>
+      <div className="product-style ">
         <h1>id: {id}</h1>
         <h4>name: {name}</h4>
         <h2>price: {price}</h2>
         <p>color-{color}</p>
+        {/* <button onClick={comment}>Add to/ card</button> */}
+        <button onClick={()=>addTocard(id)}>Add to card</button>
       </div>
     );
 };
