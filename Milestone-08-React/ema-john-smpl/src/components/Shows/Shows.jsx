2ee4c74abfc063ea46e2f5ddcea8633f2c2e808a
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-03-29 06:35:52 +0600

    start

--- Milestone-08-React/ema-john-smpl/src/components/Shows/Shows.jsx
+++ Milestone-08-React/ema-john-smpl/src/components/Shows/Shows.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Ordercard from '../Order-card/Ordercard';
 import Product from '../product/Product';
 import './Shows.css'
 const Shows = () => {
     const [products,setProducts] = useState([]);
     const [cart,setCart] = useState([]);
-
+console.log(cart);
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
+    },[])
+    useEffect(()=>{
+    const stoardCart =  getShoppingCart();
+    console.log(stoardCart);
     },[])
     //! ------------------
 const addOnclickTocard = (product) =>{
     // console.log('adddpro',product.id,product)
     const newCart = [...cart,product];
     setCart(newCart);
+    addToDb(product.id)
 
 }
     return (
