0c159f10e26dcd26252789b36f9db0bcc1cbd2d1
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-04-06 03:39:10 +0600

    style

--- Milestone-08-React/price-list-cards/src/components/Nav-bar/Navbar.jsx
+++ Milestone-08-React/price-list-cards/src/components/Nav-bar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     ];
     return (
       <nav>
-        <div className='md:hidden'  onClick={() => setOpen(!open)}>
+        <div className="md:hidden" onClick={() => setOpen(!open)}>
           <span>
             {open === true ? (
               <XMarkIcon className="h-6 w-6 text-blue-500 mt-5" />
@@ -43,7 +43,7 @@ const Navbar = () => {
           </span>
         </div>
 
-        <ul className="md:flex gap-5">
+        <ul className={`md:flex gap-5 absolute md:static mt-2  duration-500 p-2 border-red-50 rounded  bg-red-300 ${open ? 'top-20' : '-top-96'}`}>
           {routes.map((route) => (
             <Link key={route.id} route={route}></Link>
           ))}
