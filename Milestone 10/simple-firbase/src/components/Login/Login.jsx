5d86d156008104695f71224d588f0ac1026244f4
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-04-24 02:30:50 +0600

    google login

--- Milestone 10/simple-firbase/src/components/Login/Login.jsx
+++ Milestone 10/simple-firbase/src/components/Login/Login.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { initializeApp } from "firebase/app";
 import app from '../../Firebase/firebase.config';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 const Login = () => {
+    const [user,setUser] = useState(null)
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-
+  
 
     const googleHandelarClick = ()=>{
       signInWithPopup(auth,provider)
       .then((result) =>{
-         const user = result.user;
-         console.log(user);
+         const signInuser = result.user;
+         console.log(signInuser);
+         setUser(signInuser)
       })
       .catch((error)=>{
         console.log("error",error.message);
@@ -19,9 +21,32 @@ const Login = () => {
 
     }
 
+    const signOuthendler = () =>{
+        signOut(auth)
+        .then(result =>{
+            setUser(null)
+            console.log(result);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+
+
+  
+    }
+
     return (
       <div>
-        <button onClick={googleHandelarClick}>Go Google</button>
+
+        { user?<button onClick={signOuthendler}>signOut</button> :
+        <button onClick={googleHandelarClick}>Go Google</button>}
+        {user && (
+          <div>
+            User: {user.displayName}
+            <p>Email : {user.email}</p>
+            <img src={user.photoURL} alt="" />
+          </div>
+        )}
       </div>
     );
 };
