5dd6a4acae77c547e99beccb058cdf8cbf57859f
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-04-27 04:56:19 +0600

    new-project-start

---
+++ Milestone 10/auth-firbase-contextr-tlwind/src/Components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>LOgin</h1>
+            
+        </div>
+    );
+};
+
+export default Login;
---
+++ Milestone 10/auth-firbase-contextr-tlwind/src/Firbase/firbase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-KwOZNW09cDo5dQJmjSVAFw4ZDwPlnLg",
+  authDomain: "auth-firbase-context.firebaseapp.com",
+  projectId: "auth-firbase-context",
+  storageBucket: "auth-firbase-context.appspot.com",
+  messagingSenderId: "790961360839",
+  appId: "1:790961360839:web:754fa941b903848ea953e2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
